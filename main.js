6b46153f9e95d12fa9b318767e5ecfcefd0ac1d4
ohad2255 <ohad2255@hotmail.com>
2017-12-02 15:51:25 +0200

    lower-gallerys-v3

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
- var mySwiper = new Swiper ('.swiper-container', {
+  new Swiper ('.main-gallery-container', {
    
     direction: 'horizontal',
     loop: true,
@@ -6,6 +6,34 @@
     pagination: {
       el: '.swiper-pagination',
     },
+
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+  new Swiper ('#mishpatiot-slider', {
+   
+    direction: 'horizontal',
+    loop: true,
+
+     pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+     },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+  new Swiper ('#logo-swiper', {
+   
+    direction: 'horizontal',
+    loop: true,
 
 
     navigation: {
